160342a485abdc86450e8e66a9f28dacc3b922f5
Pieter Botha <218000847@stud.cut.ac.za>
2021-01-19 18:21:03 +0200

    Forgot to branch

---
+++ TableFindBackend/ViewModels/RestaurantView.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace TableFindBackend.ViewModels
+{
+    partial class RestaurantView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLocation = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.lblLocation);
+            this.panel1.Controls.Add(this.lblName);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(276, 53);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(3, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(149, 21);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Restaurant Name";
+            // 
+            // lblLocation
+            // 
+            this.lblLocation.AutoSize = true;
+            this.lblLocation.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLocation.Location = new System.Drawing.Point(4, 31);
+            this.lblLocation.Name = "lblLocation";
+            this.lblLocation.Size = new System.Drawing.Size(128, 17);
+            this.lblLocation.TabIndex = 1;
+            this.lblLocation.Text = "Restaurant Location";
+            // 
+            // RestaurantView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "RestaurantView";
+            this.Size = new System.Drawing.Size(276, 53);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.Label lblName;
+    }
+}
---
+++ TableFindBackend/ViewModels/RestaurantView.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TableFindBackend.ViewModels
+{
+    public partial class RestaurantView : UserControl
+    {
+        public RestaurantView()
+        {
+            InitializeComponent();
+        }
+        //#region properties
+        //private string uName;
+        //private string uContactNumber;
+        //private string tTableName;
+        //private string tDate;
+        //private string tTime;
+        //private string objectId;
+        //private string uObjectId;
+        //private bool blinking = true;
+
+        //[Category("Custom Prop")]
+        //public string UserName
+        //{
+        //    get => uName; set
+        //    {
+        //        uName = value;
+        //        lblName.Text = value;
+        //    }
+        //}
+        //[Category("Custom Prop")]
+        //public string UserContactNumber
+        //{
+        //    get => uContactNumber; set
+        //    {
+        //        uContactNumber = value;
+        //        lblContact.Text = value;
+        //    }
+        //}
+        //[Category("Custom Prop")]
+        //public string TableName
+        //{
+        //    get => tTableName; set
+        //    {
+        //        tTableName = value;
+        //        lblTable.Text = value;
+        //    }
+        //}
+        //[Category("Custom Prop")]
+        //public string Date
+        //{
+        //    get => tDate; set
+        //    {
+        //        tDate = value;
+        //        lblDate.Text = value;
+        //    }
+        //}
+        //[Category("Custom Prop")]
+        //public string FromToTime
+        //{
+        //    get => tTime; set
+        //    {
+        //        tTime = value;
+        //        lblTime.Text = value;
+        //    }
+        //}
+        //[Category("Custom Prop")]
+        //public string ObjectId
+        //{
+        //    get => objectId; set
+        //    {
+        //        objectId = value;
+        //        lblId.Text = value;
+        //    }
+        //}
+        //[Category("Custom Prop")]
+        //public string UObjectId { get => uObjectId; set => uObjectId = value; }
+
+
+        //#endregion
+    }
+}
