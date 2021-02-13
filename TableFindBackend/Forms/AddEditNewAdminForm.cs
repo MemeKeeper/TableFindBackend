76c2baa1c5c8343b1e8d72589b19f24ec8f40686
Pieter Botha <218000847@stud.cut.ac.za>
2021-02-13 20:40:11 +0200

    Added more code reusability to the AddEditAdminPinsForm

--- TableFindBackend/Forms/AddEditNewAdminForm.cs
+++ TableFindBackend/Forms/AddEditNewAdminForm.cs
@@ -67,125 +67,132 @@ namespace TableFindBackend.Forms
         {
             showLoading(true);
 
+            #region callbacks are kept here to rest
             AsyncCallback<AdminPins> reactivateObjectCallback = new AsyncCallback<AdminPins>(
-                  savedAdminPins =>
-                  {
-                      Invoke(new Action(() =>
-                      {
-                          showLoading(false);
-                          OwnerStorage.FileWriter.WriteLineToFile("User reactivated Admin ", true);
-                          OwnerStorage.FileWriter.WriteLineToFile("Name:  " + TempAdmin.UserName, false);
-                          OwnerStorage.LogInfo.Add(TempAdmin.UserName + " admin User was reactivated");
-                          OwnerStorage.LogTimes.Add(System.DateTime.Now.ToString("HH:mm:ss"));
-                          MessageBox.Show(this, TempAdmin.UserName + " has been reactivated");
-                          DialogResult = DialogResult.OK;
-                          this.Close();
-                      }));
-                  },
-                  error =>
-                  {
-                      Invoke(new Action(() =>
-                      {
-                          showLoading(false);
-                          MessageBox.Show(this, "Error: " + error.Message);
-                      }));
-                  });
-
-            AsyncCallback<AdminPins> saveReactivatedObjectCallback = new AsyncCallback<AdminPins>(
-              savedAdminPins =>
-              {
-                  Backendless.Persistence.Of<AdminPins>().Save(savedAdminPins, reactivateObjectCallback);
-              },
-              error =>
-              {
-                  Invoke(new Action(() =>
-                  {
-                      showLoading(false);
-                      MessageBox.Show(this, "Error: " + error.Message);
-                  }));
-              });
-            AsyncCallback<AdminPins> callback = new AsyncCallback<AdminPins>(
-                result =>
+                savedAdminPins =>
                 {
-                    //Perform other operations that indicate the success of the creation (logging, showing a message, closing the form)
-                    OwnerStorage.ListOfAdmins.Add(result);
+                    //Has been reactivated successfully, will then log all information and close the form
                     //Runs visual aspects on a new thread because you can not alter visual aspects on any thread other than the GUI thread
                     Invoke(new Action(() =>
                     {
-                        // object has been saved
                         showLoading(false);
-                        MessageBox.Show(this, "Admin User has been successfully created.");
-                        OwnerStorage.LogInfo.Add(TempAdmin.UserName + " admin User was Created");
+                        OwnerStorage.FileWriter.WriteLineToFile("User reactivated Admin ", true);
+                        OwnerStorage.FileWriter.WriteLineToFile("Name:  " + TempAdmin.UserName, false);
+                        OwnerStorage.LogInfo.Add(TempAdmin.UserName + " admin User was reactivated");
+                        OwnerStorage.LogTimes.Add(System.DateTime.Now.ToString("HH:mm:ss"));
+                        MessageBox.Show(this, TempAdmin.UserName + " has been reactivated");
+                        DialogResult = DialogResult.OK;
+                        this.Close();
+                    }));
+                },
+                error =>
+                {
+                    //Runs visual aspects on a new thread because you can not alter visual aspects on any thread other than the GUI thread
+                    Invoke(new Action(() =>
+                    {
+                        showLoading(false);
+                        MessageBox.Show(this, "Error: " + error.Message);
+                    }));
+                });
+
+        AsyncCallback<AdminPins> saveReactivatedObjectCallback = new AsyncCallback<AdminPins>(
+            savedAdminPins =>
+            {
+                //The object has to be saved first in order to be updated
+                Backendless.Persistence.Of<AdminPins>().Save(savedAdminPins, reactivateObjectCallback);
+            },
+            error =>
+            {
+                //Runs visual aspects on a new thread because you can not alter visual aspects on any thread other than the GUI thread
+                Invoke(new Action(() =>
+                {
+                    showLoading(false);
+                    MessageBox.Show(this, "Error: " + error.Message);
+                }));
+            });
+        AsyncCallback<AdminPins> callback = new AsyncCallback<AdminPins>(
+            result =>
+            {
+                //Perform other operations that indicate the success of the creation (logging, showing a message, closing the form)
+                OwnerStorage.ListOfAdmins.Add(result);
+                //Runs visual aspects on a new thread because you can not alter visual aspects on any thread other than the GUI thread
+                Invoke(new Action(() =>
+                {
+                    // object has been saved
+                    showLoading(false);
+                    MessageBox.Show(this, "Admin User has been successfully created.");
+                    OwnerStorage.LogInfo.Add(TempAdmin.UserName + " admin User was Created");
+                    OwnerStorage.LogTimes.Add(System.DateTime.Now.ToString("HH:mm:ss"));
+                    this.DialogResult = DialogResult.OK;
+                }));
+            },
+
+            fault =>
+            {
+                //Perform other operations that indicate the failure of the creation (showing a message)
+                //Runs visual aspects on a new thread because you can not alter visual aspects on any thread other than the GUI thread
+                Invoke(new Action(() =>
+                {
+                    // server reported an error
+                    showLoading(false);
+                    MessageBox.Show(this, "error: " + fault.Message);
+                }));
+            });
+
+
+            AsyncCallback<AdminPins> updateObjectCallback = new AsyncCallback<AdminPins>(
+                savedAdminPin =>
+                {
+                    //Admin has been successfully created
+                    //Runs visual aspects on a new thread because you can not alter visual aspects on any thread other than the GUI thread
+                    Invoke(new Action(() =>
+                    {
+                        showLoading(false);
+                        MessageBox.Show(this, "Admin PIN has been updated");
+                        OwnerStorage.LogInfo.Add(TempAdmin.UserName + " admin User was Updated");
                         OwnerStorage.LogTimes.Add(System.DateTime.Now.ToString("HH:mm:ss"));
                         this.DialogResult = DialogResult.OK;
                     }));
                 },
-
-                fault =>
+                error =>
                 {
-                    //Perform other operations that indicate the failure of the creation (showing a message)
                     //Runs visual aspects on a new thread because you can not alter visual aspects on any thread other than the GUI thread
                     Invoke(new Action(() =>
                     {
-                        // server reported an error
+                        //server reported an error
                         showLoading(false);
-                        MessageBox.Show(this, "error: " + fault.Message);
+                        MessageBox.Show(this, "error: " + error.Message);
                     }));
                 });
 
-
-                AsyncCallback<AdminPins> updateObjectCallback = new AsyncCallback<AdminPins>(
-                    savedAdminPin =>
-                    {
-                        //Admin has been successfully created
+            AsyncCallback<AdminPins> saveObjectCallback = new AsyncCallback<AdminPins>(
+                savedAdminPin =>
+                {
+                        //The Object has to be saved first in order to update the object
+                        Backendless.Persistence.Of<AdminPins>().Save(savedAdminPin, updateObjectCallback);
+                },
+                error =>
+                {
+                        //server reported an error
                         //Runs visual aspects on a new thread because you can not alter visual aspects on any thread other than the GUI thread
                         Invoke(new Action(() =>
-                        {
-                            showLoading(false);
-                            MessageBox.Show(this, "Admin PIN has been updated");
-                            OwnerStorage.LogInfo.Add(TempAdmin.UserName + " admin User was Updated");
-                            OwnerStorage.LogTimes.Add(System.DateTime.Now.ToString("HH:mm:ss"));
-                            this.DialogResult = DialogResult.OK;
-                        }));
-                    },
-                    error =>
                     {
-                        //Runs visual aspects on a new thread because you can not alter visual aspects on any thread other than the GUI thread
-                        Invoke(new Action(() =>
-                        {
-                            //server reported an error
-                            showLoading(false);
-                            MessageBox.Show(this, "error: " + error.Message);
-                        }));
-                    });
+                        showLoading(false);
+                        MessageBox.Show(this, "error: " + error.Message);
+                    }));
+                });
 
-                AsyncCallback<AdminPins> saveObjectCallback = new AsyncCallback<AdminPins>(
-                  savedAdminPin =>
-                  {
-                          //The Object has to be saved first in order to update the object
-                          Backendless.Persistence.Of<AdminPins>().Save(savedAdminPin, updateObjectCallback);
-                  },
-                  error =>
-                  {
-                          //server reported an error
-                          //Runs visual aspects on a new thread because you can not alter visual aspects on any thread other than the GUI thread
-                          Invoke(new Action(() =>
-                      {
-                          showLoading(false);
-                          MessageBox.Show(this, "error: " + error.Message);
-                      }));
-                  });
+            #endregion
 
-
-                if (tbxName.Text == ""
-                    || tbxContact.Text == ""
-                    || tbxPinCode.Text == "")//performs Validation to determine if all textBoxes are filled in
-                {
-                    showLoading(false);
-                    MessageBox.Show(this, "Please fill in all fields.");
-                }
-                else
-                {
+            if (tbxName.Text == ""
+                || tbxContact.Text == ""
+                || tbxPinCode.Text == "")//performs Validation to determine if all textBoxes are filled in
+            {
+                showLoading(false);
+                MessageBox.Show(this, "Please fill in all fields.");
+            }
+            else
+            {
                 if (tbxContact.TextLength == 10)//performs Contact validation
                 {
                     if (tbxPinCode.TextLength < 4)//performs Validation to determine if the pin is at least 4 digits long
@@ -250,12 +257,12 @@ namespace TableFindBackend.Forms
                         else
                         {
                             //Edit Existing Admin
-                            if (TempAdmin.Active == false)
+                            if (TempAdmin.Active == false)//determines whether the admin should be reactivated or just edited
                             {
                                 TempAdmin.Active = true;
                                 Backendless.Persistence.Of<AdminPins>().Save(TempAdmin, saveReactivatedObjectCallback);
                             }
-                            else
+                            else//Admin is to be updated
                             {
                                 bool flag = false;
                                 foreach (AdminPins a in OwnerStorage.ListOfAdmins)
@@ -267,7 +274,7 @@ namespace TableFindBackend.Forms
                                     }
                                 }
 
-                                if (flag == false)
+                                if (flag == false)//flag is used to determine of the pin specified by the user already exists. The way the program tracks which user logs in is his/her PinCode, therefor it has to be unique
                                 {
                                     TempAdmin.UserName = tbxName.Text;
                                     TempAdmin.ContactNumber = tbxContact.Text;
@@ -277,6 +284,7 @@ namespace TableFindBackend.Forms
                                 }
                                 else
                                 {
+                                    //a admin with this pin already exists
                                     showLoading(false);
                                     MessageBox.Show(this, "There is already an administrator with this PIN, please use a different PIN");
                                     tbxPinCode.Text = "";
@@ -288,7 +296,6 @@ namespace TableFindBackend.Forms
                     }
                 }
             }
-                         
         }
 
         private void btnCancel_Click(object sender, EventArgs e)//Button used to close the form if the user wishes to not save changes made
@@ -299,20 +306,17 @@ namespace TableFindBackend.Forms
 
         private void tbxContact_KeyPress(object sender, KeyPressEventArgs e)
         {
+            //ensures that only digits can be entered into the contact TextBox
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
                (e.KeyChar != '.'))
             {
                 e.Handled = true;
             }
-
-            // only allow one decimal point
-            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
-            {
-                e.Handled = true;
-            }
         }
+        
         private void showLoading(bool toggle)
         {
+            //a method that will appear on all forms. it simulates a loading screen by showing and hiding all neccessary buttons and interface elements.
             if (toggle == true)
             {
                 pbxLoading.Visible = true;
@@ -333,9 +337,10 @@ namespace TableFindBackend.Forms
         }
         private void btnRemoveAdmin_Click(object sender, EventArgs e)
         {
-
+            //this button is multifunctional, it can both deactivate a admin object AND remove it
             if (TempAdmin.Active == false)
             {
+                //This will permanently remove the deactivated record off of the database
                 DialogResult result = MessageBox.Show(this, "Are you sure you wish permanently remove " + TempAdmin.UserName + " as administrator?", "Removing Admin", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
                 if (result == DialogResult.Yes)
                 {
@@ -343,6 +348,7 @@ namespace TableFindBackend.Forms
                     AsyncCallback<long> deleteObjectCallback = new AsyncCallback<long>(
                     deletionTime =>
                     {
+                        //success, the program will now log the event, show a success message and close the form
                         //Runs visual aspects on a new thread because you can not alter visual aspects on any thread other than the GUI thread
                         Invoke(new Action(() =>
                         {
@@ -361,6 +367,7 @@ namespace TableFindBackend.Forms
                     },
                 error =>
                 {
+                    //Something went wrong, na error message will now be displayed
                     //Runs visual aspects on a new thread because you can not alter visual aspects on any thread other than the GUI thread
                     Invoke(new Action(() =>
                     {
@@ -372,11 +379,12 @@ namespace TableFindBackend.Forms
                     AsyncCallback<AdminPins> saveObjectCallback = new AsyncCallback<AdminPins>(
                       savedAdmin =>
                       {
-
+                          //the object has to be saved first in order to be deleted
                           Backendless.Persistence.Of<AdminPins>().Remove(savedAdmin, deleteObjectCallback);
                       },
                       error =>
                       {
+                          //Something went wrong, an error message will now be displayed
                           //Runs visual aspects on a new thread because you can not alter visual aspects on any thread other than the GUI thread
                           Invoke(new Action(() =>
                           {
@@ -385,7 +393,7 @@ namespace TableFindBackend.Forms
                           }));
                       }
                     );
-
+                    //runs the callback which removes the object
                     Backendless.Persistence.Of<AdminPins>().Save(TempAdmin, saveObjectCallback);
                 }
             }
@@ -399,6 +407,7 @@ namespace TableFindBackend.Forms
                     AsyncCallback<AdminPins> updateObjectCallback = new AsyncCallback<AdminPins>(
                     savedAdminPins =>
                     {
+                        //success, the program will now log the event, show a success message and close the form
                         //Runs visual aspects on a new thread because you can not alter visual aspects on any thread other than the GUI thread
                         Invoke(new Action(() =>
                         {
@@ -414,6 +423,7 @@ namespace TableFindBackend.Forms
                     },
                     error =>
                     {
+                        //Something went wrong, an error message will now be displayed
                         //Runs visual aspects on a new thread because you can not alter visual aspects on any thread other than the GUI thread
                         Invoke(new Action(() =>
                         {
@@ -436,6 +446,7 @@ namespace TableFindBackend.Forms
                               MessageBox.Show(this, "Error: " + error.Message);
                           }));
                       });
+                    //makes the small ajustment that "Deactivates" a admin
                     TempAdmin.Active = false;
                     Backendless.Persistence.Of<AdminPins>().Save(TempAdmin, saveObjectCallback);
                 }
@@ -444,6 +455,7 @@ namespace TableFindBackend.Forms
 
         private void AddEditNewAdminForm_FormClosing(object sender, FormClosingEventArgs e)
         {
+            //this wonderfull piece of code blocks the "alt F4" capability so that the user can not close the program while a process is running
             if (e.CloseReason == System.Windows.Forms.CloseReason.UserClosing && pbxLoading.Visible == true)
             {
                 e.Cancel = true;
